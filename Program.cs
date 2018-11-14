bfb5f70e2d8aee778669c3efb56c142ed27d6dbc
Eric Almeida <37628478+almeieric@users.noreply.github.com>
2018-11-13 22:07:08 -0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,13 +15,13 @@ namespace ConsoleApp1
             vetor[pos1] = vetor[pos2];
             vetor[pos2] = aux;
 
-              
+
         }
         static void Imprime(int[] vetor)
         {
             for (int i = 0; i < vetor.Length; i++)
                 Console.Write(vetor[i] + "\t");
-                Console.WriteLine();
+            Console.WriteLine();
         }
 
         static void Main(string[] args)
@@ -31,29 +31,39 @@ namespace ConsoleApp1
             Console.ForegroundColor = ConsoleColor.Black;
 
 
-            int[] vetor = new int[] { 10, 351, 3, 1500,50 };
+            int[] vetor = new int[] { 10, 351, 3, 1500, 50 };
 
-            
-            Console.WriteLine("Função troca de posições\nInsira primeira posição: ");
-            int insPos1 = Int32.Parse(Console.ReadLine());
-            
-            Console.WriteLine("Insira segunda posição posição: ");
-            int insPos2 = Int32.Parse(Console.ReadLine());
-            
+            Imprime(vetor);//vetor não alterado
 
-            if(insPos1 > vetor.Length || insPos2 > vetor.Length)
+            int insPos1;
+            int insPos2;
+            do
             {
-                Console.WriteLine("Posição não existe");
-            }
-            else
-            {
-                Console.WriteLine("Seu primeiro numero é: " + vetor[insPos1] + " e Esta na posição: " + insPos1);
-                Console.WriteLine("Seu Segundo numero é: " + vetor[insPos2] + " e Esta na posição: " + insPos2);
 
-                Imprime(vetor);//vetor não alterado
+                Console.WriteLine("Função troca de posições\nInsira primeira posição: ");
+                insPos1 = Int32.Parse(Console.ReadLine());
+
+
+            } while (insPos1 < 0 || insPos1 > vetor.Length);
+            Console.WriteLine("Seu primeiro numero é: " + vetor[insPos1] + " e Esta na posição: " + insPos1);
+
+
+
+            do
+            {
+                Console.WriteLine("Insira segunda posição posição: ");
+                insPos2 = Int32.Parse(Console.ReadLine());
+            } while (insPos2 < 0 || insPos2 > vetor.Length);
+                                
+           
+           Console.WriteLine("Seu Segundo numero é: " + vetor[insPos2] + " e Esta na posição: " + insPos2);
+          
+        
+
+                           
                 Troca(vetor, insPos1, insPos2);//comando de alteração
                 Imprime(vetor);//vetor alterado
-            }
+            
 
 
 
